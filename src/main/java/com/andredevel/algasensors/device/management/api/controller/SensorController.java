b9da009c7ea015f68a47829f6e58de16ed4c2f1c
Andre goncalves <andrevilanovagon@hotmail.com>
2025-04-30 19:20:46 -0300

    feat: add controller and repository (sensor)

--- src/main/java/com/andredevel/algasensors/device/management/api/controller/SensorController.java
+++ src/main/java/com/andredevel/algasensors/device/management/api/controller/SensorController.java
@@ -1,31 +1,75 @@
 package com.andredevel.algasensors.device.management.api.controller;
 
-import com.andredevel.algasensors.device.management.api.model.Sensor;
 import com.andredevel.algasensors.device.management.api.model.SensorInput;
+import com.andredevel.algasensors.device.management.api.model.SensorOutput;
 import com.andredevel.algasensors.device.management.common.IdGenerator;
+import com.andredevel.algasensors.device.management.domain.model.Sensor;
+import com.andredevel.algasensors.device.management.domain.model.SensorId;
+import com.andredevel.algasensors.device.management.domain.repository.SensorRepository;
+import io.hypersistence.tsid.TSID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/sensors")
+@RequiredArgsConstructor
 public class SensorController {
+    
+    private final SensorRepository sensorRepository;
+    
+    @GetMapping
+    public Page<SensorOutput> search(@PageableDefault Pageable pageable) {
+        Page<Sensor> sensors = sensorRepository.findAll(pageable);
+        return sensors.map(this::convertToModel);
+    }
+    
+    @GetMapping("{sensorId}")
+    public SensorOutput getOne(@PathVariable TSID sensorId) {
+        Sensor sensor = sensorRepository.findById(new SensorId(sensorId))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        return convertToModel(sensor);
+    }
     
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Sensor create(@RequestBody SensorInput input) {
+    public SensorOutput create(@RequestBody SensorInput input) {
 
-        return Sensor.builder()
-                .id(IdGenerator.generateTSID())
+        Sensor sensor = Sensor.builder()
+                .id(new SensorId(IdGenerator.generateTSID()))
                 .name(input.getName())
                 .ip(input.getIp())
                 .location(input.getLocation())
                 .protocol(input.getProtocol())
                 .model(input.getModel())
                 .enabled(false)
+                .build();
+        
+        sensor = sensorRepository.saveAndFlush(sensor);
+        
+        return convertToModel(sensor);
+    }
+
+    private SensorOutput convertToModel(Sensor sensor) {
+        return SensorOutput.builder()
+                .id(sensor.getId().getValue())
+                .name(sensor.getName())
+                .ip(sensor.getIp())
+                .location(sensor.getLocation())
+                .protocol(sensor.getProtocol())
+                .model(sensor.getModel())
+                .enabled(sensor.getEnabled())
                 .build();
     }
 }
