f144172b9b45745c6b88b3bd1e441c88d76b46db
Andre goncalves <andrevilanovagon@hotmail.com>
2025-04-30 19:20:03 -0300

    feat: add SensorOutput

--- src/main/java/com/andredevel/algasensors/device/management/api/model/SensorOutput.java
+++ src/main/java/com/andredevel/algasensors/device/management/api/model/SensorOutput.java
@@ -1,16 +1,12 @@
 package com.andredevel.algasensors.device.management.api.model;
 
 import io.hypersistence.tsid.TSID;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Builder
-public class Sensor {
+public class SensorOutput {
     private TSID id;
     private String name;
     private String ip;
